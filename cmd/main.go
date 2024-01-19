cc8dd7e62a930d4aa74dce15eade6f0832d79575
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-01-19 20:13:59 +0700

    create new instance

--- cmd/main.go
+++ cmd/main.go
@@ -2,37 +2,13 @@ package main
 
 import (
 	"context"
+	"dododo/db"
 	"fmt"
-	"log"
-	"os"
-
-	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
+	db := db.New()
+	defer db.Close(context.Background())
 
-	rows, err := conn.Query(context.Background(), "SELECT * FROM users")
-	if err != nil {
-		log.Fatalf("Error executing query: %v\n", err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var name string
-		var age int
-
-		if err := rows.Scan(&id, &name, &age); err != nil {
-			log.Fatalf("Error scanning row: %v\n", err)
-		}
-
-		fmt.Printf("ID: %d, Name: %s, OtherColumn: %v\n", id, name, age)
-	}
-
+	fmt.Println("Hello World.")
 }
--- db/db.go
+++ db/db.go
@@ -1,6 +1,18 @@
 package db
 
+import (
+	"context"
+	"log"
+	"os"
 
-func New() {
-	
+	"github.com/jackc/pgx/v5"
+)
+
+
+func New() *pgx.Conn  {
+	db, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Println("Unable to connect to database: \n", err)
+	}
+	return db
 }
